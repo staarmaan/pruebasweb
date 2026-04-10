fcbb24bd62e17aa95b60c98677e02f23407c6145
marco <marcorico2002@gmail.com>
2026-04-09 21:07:33 -0700

    Instalando y configurando Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 
 // https://vite.dev/config/
@@ -9,4 +10,7 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
+    test: {
+        environment: 'jsdom'
+      }
 })
